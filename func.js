cd9e98419c03c122ff6be2cf5215d24b8a0a76f0
Shaun Smith <shaun.smith@oracle.com>
2018-11-14 12:10:22 -0500

    initial

---
+++ func.js
@@ -0,0 +1,40 @@
+const fdk = require('@fnproject/fdk');
+const im = require('imagemagick');
+const fs = require('fs');
+const tmp = require('tmp');
+const multipart = require('parse-multipart');
+
+fdk.handle(extractDimensions, { inputMode: 'buffer' });
+
+function extractDimensions(buffer, ctx) {
+  return new Promise((resolve, reject) => {
+    let boundary = getMultiPartBoundrySeparator(ctx);
+    let parts = multipart.Parse(buffer, boundary);
+    if (parts.length > 0) { // we have at least one file
+      let part = parts[0];  // process the first file only
+      tmp.tmpName((err, tmpFile) => {
+        if (err) throw err;
+        fs.writeFile(tmpFile, part.data, (err) => {
+          if (err) throw err;
+          im.identify(['-format', '{"file": "' + part.filename + '", ' + '"width": %w, "height": %h}', tmpFile],
+            (err, output) => {
+              if (err) throw err;
+              resolve(JSON.parse(output));
+            }
+          );
+        });
+      });
+    }
+  });
+}
+
+function getMultiPartBoundrySeparator(ctx) {
+  let contentType = ctx.getHeader("Content-Type");
+  if (contentType) {
+    let boundaryStart = contentType.indexOf("boundary=") + "boundary=".length;
+    if (boundaryStart > -1) {
+      return contentType.substring(boundaryStart, contentType.length);
+    }
+  }
+  throw "multipart/form-data expected";
+}
